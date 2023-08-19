8393dc08b4aafdf62397bfeae013726a0cfa3320
void2610 <izumisan2610@gmail.com>
2023-08-19 21:55:33 +0900

    Update TCPClient.ino

--- spresense_sketch/TCPClient/TCPClient.ino
+++ spresense_sketch/TCPClient/TCPClient.ino
@@ -16,9 +16,11 @@
 */
 
 #include <TelitWiFi.h>
+#include <CCS811.h>
 #include "config.h"
 
 #define  CONSOLE_BAUDRATE  115200
+#define MAX_LENGTH 10
 
 const uint8_t TCP_Data[] = "GS2200 TCP Client Data Transfer Test.";
 const uint16_t TCP_RECEIVE_PACKET_SIZE = 1500;
@@ -26,6 +28,7 @@ uint8_t TCP_Receive_Data[TCP_RECEIVE_PACKET_SIZE] = {0};
 
 TelitWiFi gs2200;
 TWIFI_Params gsparams;
+CCS811 sensor;
 
 static void led_onoff(int num, bool stat)
 {
@@ -77,6 +80,27 @@ static void led_effect(void)
 		
 }
 
+struct IntArray{
+    uint8_t array[MAX_LENGTH + 1];
+    int length;
+};
+
+struct IntArray convert(int value) {
+    IntArray result;
+    result.length = 0;
+    int v = value;
+    while (v != 0) {
+        v /= 10;
+        result.length++;
+    }
+    for (int i = 0; i < result.length; i++) {
+        result.array[result.length - i - 1] = '0' + value % 10;
+        value /= 10;
+    }
+    result.array[result.length] = '\0';
+    return result;
+}
+
 void setup() {
 	/* initialize digital pin of LEDs as an output. */
 	pinMode(LED0, OUTPUT);
@@ -105,6 +129,12 @@ void setup() {
 		while(1);
 	}
 	digitalWrite(LED0, HIGH); // turn on LED
+  
+  while(sensor.begin() != 0){
+    Serial.println("failed to init chip, please check if the chip connection is fine");
+    delay(1000);
+  }
+  sensor.setMeasCycle(sensor.eCycle_250ms);
 }
 
 // the loop function runs over and over again forever
@@ -114,10 +144,11 @@ void loop() {
 	bool served = false;
 	uint32_t timer=0;
 	int receive_size = 0;
+
+  IntArray DATA;
 	while (1) {
 		if (!served) {
 			// Start a TCP client
-			ConsoleLog("Start TCP Client");
 			server_cid = gs2200.connect(TCPSRVR_IP, TCPSRVR_PORT);
 			ConsolePrintf("server_cid: %d \r\n", server_cid);
 			if (server_cid == ATCMD_INVALID_CID) {
@@ -132,15 +163,21 @@ void loop() {
 
 			// Start the infinite loop to send the data
 			while (1) {
-        TCP_Data = String(count++);
-				if (false == gs2200.write(server_cid, TCP_Data, strlen((const char*)TCP_Data))) {
-					// Data is not sent, we need to re-send the data
-					delay(10);
-				}
+        delay(1000);
+        if(sensor.checkDataReady() == true){
+          DATA = convert(sensor.getCO2PPM());
+          Serial.println(sensor.getCO2PPM());
+          if (false == gs2200.write(server_cid, DATA.array, strlen((const char*)DATA.array))) {
+            // Data is not sent, we need to re-send the data
+            Serial.println("faild");
+            delay(10);
+          }
+        }
+        //600超えると0000000になる
+
 				while (gs2200.available()) {
 					receive_size = gs2200.read(server_cid, TCP_Receive_Data, TCP_RECEIVE_PACKET_SIZE);
 					if (0 < receive_size) {
-						ConsolePrintf("Receive %d byte:%s \r\n", receive_size, TCP_Receive_Data);
 						memset(TCP_Receive_Data, 0, TCP_RECEIVE_PACKET_SIZE);
 						WiFi_InitESCBuffer();
 					}
