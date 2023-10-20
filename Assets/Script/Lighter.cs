e1d4aa01b4c6fe654e525212acc772db69b4016f
void2610 <izumisan2610@gmail.com>
2023-10-20 15:19:09 +0900

    仮

--- Assets/Script/Lighter.cs
+++ Assets/Script/Lighter.cs
@@ -6,19 +6,54 @@ public class Lighter : MonoBehaviour
 {
     [SerializeField]
     private ParticleSystem ps;
+    [SerializeField]
+    GameObject rightHand;
+    [SerializeField]
+    GameObject leftHand;    
+
+    private float distanceToRightHand = 999;
+    private float distanceToLeftHand = 999;
+
+    void Start(){
+        ps.Stop();
+    }
 
     void Update()
     {
-        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstick, OVRInput.Controller.LTouch) || OVRInput.Get(OVRInput.Button.PrimaryThumbstick, OVRInput.Controller.RTouch))
-        {
-            if(!ps.isPlaying){
-                ps.Play();
+        
+            if (OVRInput.Get(OVRInput.Button.PrimaryThumbstick, OVRInput.Controller.LTouch))
+            {
+                if(distanceToLeftHand < 0.08f){
+                    if(!ps.isPlaying){
+                        ps.Play();
+                    }
+                }
             }
-        }
-        else{
-            if(ps.isPlaying){
-                ps.Stop();
+            else{
+                if(ps.isPlaying){
+                    ps.Stop();
+                }
             }
-        }
+        
+            if (OVRInput.Get(OVRInput.Button.PrimaryThumbstick, OVRInput.Controller.RTouch))
+            {
+                if(distanceToRightHand < 1.1f){
+                    if(!ps.isPlaying){
+                        ps.Play();
+                    }
+                }
+            }
+            else{
+                if(ps.isPlaying){
+                    ps.Stop();
+                }
+            }
+
+    }
+
+    void FixedUpdate() {
+        distanceToLeftHand = Vector3.Distance(this.transform.position, leftHand.transform.position);
+        distanceToRightHand = Vector3.Distance(this.transform.position, rightHand.transform.position);
+        Debug.Log(distanceToRightHand);
     }
 }
